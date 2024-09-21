7ebfc876f544804e1e0def8979fa29799c703fb5
pogch <pogchamb1@gmail.com>
2024-09-21 13:55:47 +0300

    fix film item view

--- app/src/main/java/com/santorence/secveniafilmapp/core/MainActivity.kt
+++ app/src/main/java/com/santorence/secveniafilmapp/core/MainActivity.kt
@@ -2,10 +2,12 @@ package com.santorence.secveniafilmapp.core
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.santorence.secveniafilmapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
